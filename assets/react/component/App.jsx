6239d00c28093c61b393c625ffbc5b781ec89f50
NOTLimai <truant.web.dev@gmail.com>
2024-04-10 19:16:51 +0200

    new call api for test

--- assets/react/component/App.jsx
+++ assets/react/component/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Page from "./Home/Page";
 
 const App = () => {
+    const [posts, setPosts] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetchDatas()
+    }, []);
+
+    const fetchDatas = async () => {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                headers: {
+                    'Accept' : "application/json"
+                }
+            }).then(r => r.json())
+
+            setPosts(response)
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div>
-            <h1>Bonjour tous le monde</h1>
             <Page/>
+            {loading && <h2>Chargement ...</h2>}
+            {!loading && <ul>
+                {posts.map(post => <li key={post.id}>{post.title}</li>)}
+            </ul>}
         </div>
     )
 }
